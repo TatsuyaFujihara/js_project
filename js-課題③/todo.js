fa13f7fc2c69815c5058054f666e25e23a931167
藤原　龍也 <fujiharatatsuya@fujiwaratatsuyanoMacBook-Pro.local>
2021-08-07 06:21:11 +0900

    js課題 3-4

--- js-課題③/todo.js
+++ js-課題③/todo.js
@@ -1,4 +1,5 @@
 const lists = [];
+// 新規todoを追加する際の処理
 function add() {
 // 入力された値、tbody内の情報を取得 ▶︎ tbodyの中を空に
     const content = document.getElementById('content');
@@ -10,24 +11,34 @@ function add() {
     roop();
 }
 
-// 配列の中身をループ処理
+
+
+// todoを表示させる処理
 function roop() {
     lists.forEach((list, i) => {   
-    // 繰り返し時のインデックス値を表示             
+        const statusId = list.status;       
         const newList = tbody.insertRow();
+        newList.classList.add('show');
+        if (statusId === 0) {
+            newList.id = 'unCompletes'+i;
+        } else if (statusId === 1){
+            newList.id = 'Completes'+i;
+        }
+        filter();
+        // インデックス値の代入 
         const numCell = newList.insertCell();
         const listNum = document.createTextNode(i);
         numCell.appendChild(listNum);
-    // 配列"lists"の"content"オブジェクトから要素を取得
+        // todoのコンテンツ用セルの追加
         const todoCell = newList.insertCell();
         const todo = document.createTextNode(list.content);
         todoCell.appendChild(todo);
-    // 配列"lists"の"status"オブジェクトの要素による、条件分岐
+        // statusButtonの設置
         const statusCell = newList.insertCell();
         const statusButt = document.createElement('button');
-        const statusId = list.status;
         statusButt.id = i;
         statusButt.addEventListener("click", {id: statusButt.id, handleEvent: status}, false);
+    // statusButtonの条件分岐    
         if (statusId === 0) {
             statusButt.textContent = '作業中';
         } else {
@@ -44,8 +55,11 @@ function roop() {
         delButt.addEventListener("click", {id: delButt.id, handleEvent: del}, false);
         delCell.appendChild(delButt);
     });   
+    content.value = '';
+    
 }
 
+// 削除ボタンが押された時の処理
 function del(id) {
     const delId= id.target.id;
     lists.splice(delId, 1);
@@ -55,6 +69,7 @@ function del(id) {
     roop();
 }
 
+// todoの完了・未完了のボタンが押された時の処理
 function status(id) {
     const statusId= id.target.id;
     const staArr= lists[statusId];
@@ -65,4 +80,69 @@ function status(id) {
     }
     tbody.innerHTML = '';
     roop();
+}
+
+// 表示切り替えテスト
+function filter() {
+    const radioButtAll = document.getElementById('all');
+    const radioButtWorking = document.getElementById('working');
+    const radioButtDone = document.getElementById('done');
+
+    const tableBody = document.getElementById('tbody');
+    const row = tableBody.rows.length;
+    for (let i = 0; i < row; i++) {
+
+        if (radioButtAll.checked) {
+            const uncompleteRow = document.getElementById('unCompletes'+i);
+                if (uncompleteRow !== null) {
+                    const unCompClass = uncompleteRow.classList.contains('displayNone');                
+                    if (unCompClass === true) {
+                        uncompleteRow.classList.replace('displayNone', 'show');
+                    }
+                }
+                const completeRow = document.getElementById('Completes'+i);
+                if (completeRow !== null) {
+                    const CompClass = completeRow.classList.contains('displayNone');                
+                    if (CompClass === true) {
+                        completeRow.classList.replace('displayNone', 'show');
+                    }
+                }
+        } else if (radioButtWorking.checked) {
+        // 完了したtodoを非表示にさせるclassを付与する
+            const completeRow = document.getElementById('Completes'+i);   
+                if (completeRow !== null) {
+                    const CompClass = completeRow.classList.contains('show');                
+                    if (CompClass === true) {
+                        completeRow.classList.replace('show', 'displayNone');
+                    }
+                }
+            // 作業中のtodoを表示させるclassを付与する
+                const uncompleteRow = document.getElementById('unCompletes'+i);
+                if (uncompleteRow !== null) {
+                    const unCompClass = uncompleteRow.classList.contains('displayNone');                
+                    if (unCompClass === true) {
+                        uncompleteRow.classList.replace('displayNone', 'show');
+                    }
+                }
+
+        } else if (radioButtDone.checked) {
+            // 完了したtodoを非表示にさせるclassを付与する
+            const uncompleteRow = document.getElementById('unCompletes'+i);   
+                if (uncompleteRow !== null) {
+                    const unCompClass = uncompleteRow.classList.contains('show');                
+                    if (unCompClass === true) {
+                        uncompleteRow.classList.replace('show', 'displayNone');
+                    }
+                }
+            // 作業中のtodoを表示させるclassを付与する
+                const completeRow = document.getElementById('Completes'+i);
+                if (completeRow !== null) {
+                    const CompClass = completeRow.classList.contains('displayNone');                
+                    if (CompClass === true) {
+                        completeRow.classList.replace('displayNone', 'show');
+                    }
+                }
+        }
+    }
 };
+   
